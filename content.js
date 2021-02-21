c809746ba40b8b5838033cd1251fbaf39699858d
ApocalypseCalculator <>
2021-02-21 18:10:10 -0500

    Update content.js

---
+++ content.js
@@ -0,0 +1,3 @@
+chrome.browserAction.setBadgeText({ text: 'ON' });
+chrome.browserAction.setBadgeBackgroundColor({ color: '#4688F1' });
+chrome.browserAction.setTitle({ title: "PnW Reworked UI is active!" });
