766968b3c239df9ef5f5de73c32eda3cc8bb7689
ApocalypseCalculator <59318806+ApocalypseCalculator@users.noreply.github.com>
2021-03-11 17:55:08 -0500

    Delete content.js

--- content.js
+++
@@ -1,3 +0,0 @@
-chrome.browserAction.setBadgeText({ text: 'ON' });
-chrome.browserAction.setBadgeBackgroundColor({ color: '#4688F1' });
-chrome.browserAction.setTitle({ title: "PnW Reworked UI is active!" });
