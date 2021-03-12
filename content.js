bbc55acf7f4b5f9d8c30c618a318b5bb52c56a51
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-03-12 09:59:08 -0500

    fix

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.getComputedStyle(document.body, null).backgroundImage.includes("linear-gradient")) {
-        document.body.style = "background: linear-gradient(to bottom, rgb(85, 72, 95) 0, rgb(76, 94, 70) 100%) !important;";
-    }
-}, false);
+if (window.getComputedStyle(document.body, null).backgroundImage.includes("linear-gradient")) {
+    document.body.style = "background: linear-gradient(to bottom, rgb(85, 72, 95) 0, rgb(76, 94, 70) 100%) !important;";
+}
